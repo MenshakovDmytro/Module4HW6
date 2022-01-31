9333e5c5a0cb66f638cb5bc05803a264aef82960
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-31 20:56:09 +0200

    added EntityConfigurators

---
+++ Module4HW6/Module4HW6/Data/EntityConfigurators/ArtistTypeConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW6.Data.Entity;
+
+namespace Module4HW6.Data.EntityConfigurators
+{
+    public class ArtistTypeConfigurator : IEntityTypeConfiguration<Artist>
+    {
+        public void Configure(EntityTypeBuilder<Artist> builder)
+        {
+            builder.ToTable("Artist");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Id).ValueGeneratedNever();
+            builder.Property(a => a.Name).IsRequired();
+            builder.Property(a => a.DateOfBirth).IsRequired();
+        }
+    }
+}
---
+++ Module4HW6/Module4HW6/Data/EntityConfigurators/GenreTypeConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW6.Data.Entity;
+
+namespace Module4HW6.Data.EntityConfigurators
+{
+    public class GenreTypeConfigurator : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.ToTable("Genre");
+
+            builder.HasKey(g => g.Id);
+
+            builder.Property(g => g.Id).ValueGeneratedNever();
+            builder.Property(g => g.Title).IsRequired();
+        }
+    }
+}
