d72d01e365b267cc3a8d54d2e3a1c9ba171602df
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-31 20:57:04 +0200

    added TransactionHelper

---
+++ Module4HW6/Module4HW6/Helper/TransactionHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Module4HW6.Data;
+
+namespace Module4HW6.Helper
+{
+    public class TransactionHelper
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionHelper(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task ExecuteTransaction(Func<Task> func)
+        {
+            await using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await func.Invoke();
+                    await transaction.CommitAsync();
+                }
+                catch (Exception)
+                {
+                    await transaction.RollbackAsync();
+                }
+            }
+        }
+    }
+}
