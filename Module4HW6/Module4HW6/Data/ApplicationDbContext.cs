0257dd405ba89a5112e8189165f9a15c1cb4f2d1
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-31 20:56:41 +0200

    added ApplicationDbContext

---
+++ Module4HW6/Module4HW6/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module4HW6.Data.Entity;
+using Module4HW6.Data.EntityConfigurators;
+
+namespace Module4HW6.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArtistTypeConfigurator());
+            modelBuilder.ApplyConfiguration(new GenreTypeConfigurator());
+            modelBuilder.ApplyConfiguration(new SongTypeConfigurator());
+        }
+    }
+}
